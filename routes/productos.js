a6d16aac992d2fae8cdd106e4bfb1070e9328aca
HectordelRio <AL07020940@tecmilenio.mx>
2026-02-18 19:31:07 -0600

    Update productos.js

--- routes/productos.js
+++ routes/productos.js
@@ -1,17 +1,17 @@
-// routes/productos.js
+
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const path = require('path');
 const prodsPath = path.join(__dirname, '../data/productos.json');
 
-// OBTENER TODOS
+//obtiene todos
 router.get('/', (req, res) => {
     const productos = JSON.parse(fs.readFileSync(prodsPath, 'utf8'));
     res.json(productos);
 });
 
-// AGREGAR
+//agrega
 router.post('/', (req, res) => {
     const { nombre, precio } = req.body;
     const productos = JSON.parse(fs.readFileSync(prodsPath, 'utf8'));
@@ -23,16 +23,16 @@ router.post('/', (req, res) => {
     res.json(nuevo);
 });
 
-// ELIMINAR (Esta es la que te faltaba)
+//elimina
 router.delete('/:id', (req, res) => {
     const { id } = req.params;
     let productos = JSON.parse(fs.readFileSync(prodsPath, 'utf8'));
     
-    // Filtramos para quitar el que tenga ese ID
     productos = productos.filter(p => p.id != id);
     
     fs.writeFileSync(prodsPath, JSON.stringify(productos, null, 2));
     res.json({ mensaje: "Eliminado" });
 });
+
 
 module.exports = router;
