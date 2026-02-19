144705d28eb5038cad20b38741c0473b1eb3ea33
HectordelRio <AL07020940@tecmilenio.mx>
2026-02-18 19:32:51 -0600

    Update server.js

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Servir archivos estáticos desde la carpeta raíz
+
 app.use(express.static(__dirname));
 
-// RUTAS (Verifica que tus archivos en /routes se llamen así)
+
 app.use('/auth', require('./routes/auth'));
 app.use('/productos', require('./routes/productos'));
 
-// Carga el HTML al entrar a localhost:3000
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
@@ -21,4 +21,5 @@ app.get('/', (req, res) => {
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`✅ Servidor vivo en: http://localhost:${PORT}`);
+
 });
